1d519228ddcf530ce314f6840064a1d1191f6659
doomling <bethlejem@gmail.com>
2023-09-06 00:23:16 -0300

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
-
 import { Open_Sans } from "@next/font/google";
+import Script from "next/script";
 
 // If loading a variable font, you don't need to specify the font weight
 const openSans = Open_Sans({
@@ -11,10 +11,28 @@ const openSans = Open_Sans({
 
 function MyApp({ Component, pageProps }) {
   return (
-    <main className={openSans.className}>
-      <Component {...pageProps} />
-      <Analytics />
-    </main>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
+
+      <main className={openSans.className}>
+        <Component {...pageProps} />
+        <Analytics />
+      </main>
+    </>
   );
 }
 
