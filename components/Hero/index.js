247e3f04e4daf093d15a85c964571c5a9843efed
doomling <bethlejem@gmail.com>
2025-07-03 21:05:08 -0300

    fix/hero tag

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -5,7 +5,7 @@ import styles from "./style.module.scss";
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>La hero tag sobre codigo, cultura y arte digital</h1>
+      <h1>Acá se crea côdigo, cultura y arte digital</h1>
       <p>Contenido tecnico, experimental y curioso para mentes inquietas</p>
       <Link href="/recursos">
         <Button primary text="Explorar recursos" />
