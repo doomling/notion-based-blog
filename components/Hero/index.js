977593c01c341378244f60f4eadcb256c8eb1c5f
doomling <bethlejem@gmail.com>
2025-07-03 22:27:32 -0300

    fix/typos and link mismatch

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -5,8 +5,8 @@ import styles from "./style.module.scss";
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>Acá se crea côdigo, cultura y arte digital</h1>
-      <p>Contenido tecnico, experimental y curioso para mentes ignquietas</p>
+      <h1>Acá se crea código, cultura y arte digital</h1>
+      <p>Contenido técnico, experimental y curioso para mentes inquietas</p>
       <Link href="/recursos">
         <Button primary text="Explorar recursos" />
       </Link>
