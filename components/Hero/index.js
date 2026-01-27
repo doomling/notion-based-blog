1629f57c7cbb6ab803d83ef1d6d6f2ad3c09cde2
doomling <bethlejem@gmail.com>
2026-01-27 20:35:54 -0300

    feat/ hero change

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -5,15 +5,23 @@ import styles from "./style.module.scss";
 export default function Hero() {
   return (
     <section className={styles.hero}>
-      <h1>Acá se crea código, cultura y arte digital</h1>
-      <p>Contenido técnico, experimental y curioso para mentes inquietas</p>
-      <Link href="/recursos">
-        <Button primary text="Explorar recursos" />
-      </Link>
-      <a href="#proyectos">
-        {" "}
-        <Button text="Trabajemos juntos" />
-      </a>
+      <div className={styles.waveContainer}>
+        <div className={styles.wave}></div>
+        <div className={styles.wave}></div>
+        <div className={styles.wave}></div>
+      </div>
+      <div className={styles.heroContent}>
+        <h1>Te ayudo a ordenar tu carrera en tecnología</h1>
+        <p>Clases, kits y recursos para dar el siguiente paso</p>
+        <div className={styles.buttons}>
+          <Link href="/recursos">
+            <Button primary text="Explorar recursos" />
+          </Link>
+          <a href="#proyectos">
+            <Button text="Trabajemos juntos" />
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
